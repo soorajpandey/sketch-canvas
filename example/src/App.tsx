83590e2c6e1ff29f5f3a365e915c289f6a5c6532
Suraj Pandey <surajpandey0016@gmail.com>
2022-08-07 22:55:51 +0900

    initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { useRef } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+ 
+import { SketchCanvas, SketchCanvasRef } from 'rn-perfect-sketch-canvas';
+import { useSnapshot } from 'valtio';
+import Header from './components/Header';
+import Toolbar from './components/Toolbar';
+import { state } from './store';
+
+export default function App() {
+  const { width } = useWindowDimensions();
+  const canvasRef = useRef<SketchCanvasRef>(null);
+  const snap = useSnapshot(state);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: '#f0f0f0',
+          flex: 1,
+          alignItems: 'center',
+        }}
+      >
+        <Header canvasRef={canvasRef} />
+        <View
+          style={{
+            width: width - 24,
+            flexGrow: 1,
+            backgroundColor: '#ffffff',
+            borderRadius: 10,
+            overflow: 'hidden',
+            elevation: 1,
+          }}
+        >
+          <SketchCanvas
+            strokeColor={snap.strokeColor}
+            strokeWidth={snap.strokeWidth}
+            ref={canvasRef}
+            containerStyle={styles.container}
+          />
+        </View>
+
+        <Toolbar />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
---
+++ src/components/SketchCanvas/types.ts
@@ -0,0 +1,38 @@
+import type { Color, ImageFormat, SkImage } from '@shopify/react-native-skia';
+import type { StyleProp, ViewStyle } from 'react-native';
+import type { CompletedPoints } from 'src/store';
+
+
+export type StrokeStyle = 'stroke' | 'fill';
+
+export interface SketchCanvasRef {
+  reset: () => void;
+  undo: () => void;
+  redo: () => void;
+  toBase64: (format?: ImageFormat, quality?: number) => string | undefined;
+  toImage: () => SkImage | undefined;
+  toSvg: (width: number, height: number, backgroundColor?: string) => string;
+  toPath: () => CompletedPoints[];
+  drawPath: (path: CompletedPoints[]) => void;
+  withdraw: () => void;
+  toPoints: () => Point[][];
+  addPoints: (points: Point[][], style?: StyleOptions) => void;
+}
+
+export interface SketchCanvasProps {
+  strokeWidth?: number;
+  strokeColor?: Color;
+  strokeStyle?: 'stroke' | 'fill';
+  containerStyle?: StyleProp<ViewStyle>;
+  children?: React.ReactNode;
+  topChildren?: React.ReactNode;
+  bottomChildren?: React.ReactNode;
+}
+
+export interface StyleOptions {
+  strokeColor?: Color;
+  strokeStyle?: 'stroke' | 'fill';
+  strokeWidth?: number;
+}
+
+export type Point = [number, number];
